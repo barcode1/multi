eb72c8d5cff3d53fc82195b620b84fcace919aa8
reza gholami <reza.gh.8958@gmail.com>
2025-05-23 12:29:09 +0330

    add

--- multi.py
+++ multi.py
@@ -7,9 +7,10 @@ def multi(a,b):
     else:
         return a*b
 
-for i in range(10):
+for i in range(1,10):
     print(multi(i,i))
-
+    if i==7:
+       print(multi(i,(i+7)))
 if __name__ == '__main__':
     num=int(input('enter a number:'))
     num2=int(input('enter another number:'))
