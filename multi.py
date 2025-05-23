93c843d8cb43b9c262c0d3bd9989971b447b6cc1
reza gholami <reza.gh.8958@gmail.com>
2025-05-23 14:58:34 +0330

    add fac

--- multi.py
+++ multi.py
@@ -11,7 +11,15 @@ for i in range(1,10):
     print(multi(i,i))
     if i==7:
        print(multi(i,(i+7)))
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
 if __name__ == '__main__':
     num=int(input('enter a number:'))
     num2=int(input('enter another number:'))
     print(multi(num,num2))
+    num = int(input("Enter a number: "))
+    print(f"Factorial of {num} is {factorial(num)}")
+
